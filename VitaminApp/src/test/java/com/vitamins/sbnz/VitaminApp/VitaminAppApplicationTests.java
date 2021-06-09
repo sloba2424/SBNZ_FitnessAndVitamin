f56b306a370c034d8a0df06eebfe28b0fc9c47cc
sloba2424 <sloba2424@gmail.com>
2021-06-09 15:06:23 +0200

    Model and dependency changes

---
+++ VitaminApp/src/test/java/com/vitamins/sbnz/VitaminApp/VitaminAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitamins.sbnz.VitaminApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaminAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
